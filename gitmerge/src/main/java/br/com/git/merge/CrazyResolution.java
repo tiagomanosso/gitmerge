92e0793e475b576d29dac84b1e0607fbf917599b
Tiago Manosso <tiagomanosso@gmail.com>
2013-06-13 13:20:39 -0300

    Adding String

--- gitmerge/src/main/java/br/com/git/merge/CrazyResolution.java
+++ gitmerge/src/main/java/br/com/git/merge/CrazyResolution.java
@@ -1,6 +1,15 @@
 package br.com.git.merge;
 
-
 public class CrazyResolution {
+
+	private String crazy;
+
+	public String getCrazy() {
+		return crazy;
+	}
+
+	public void setCrazy(String crazy) {
+		this.crazy = crazy;
+	}
 
 }
