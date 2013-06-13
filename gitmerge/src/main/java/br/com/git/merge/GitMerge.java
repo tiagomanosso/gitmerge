f9c400d1adcec1368ecc6137475dd1593eb04595
Tiago Manosso <tiagomanosso@gmail.com>
2013-06-12 21:05:33 -0300

    GitMerge class

--- gitmerge/src/main/java/br/com/git/merge/GitMerge.java
+++ gitmerge/src/main/java/br/com/git/merge/GitMerge.java
@@ -2,4 +2,14 @@ package br.com.git.merge;
 
 public class GitMerge {
 
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
