ad53174de80995ea94be7fc436a5872234bd1a9e
Tiago Manosso <tiagomanosso@gmail.com>
2013-06-13 13:44:08 -0300

    Boolean

--- gitmerge/src/main/java/br/com/git/merge/ConvertTo.java
+++ gitmerge/src/main/java/br/com/git/merge/ConvertTo.java
@@ -4,6 +4,16 @@ public class ConvertTo {
 
 	private String description;
 
+	private boolean option;
+
+	public boolean isOption() {
+		return option;
+	}
+
+	public void setOption(boolean option) {
+		this.option = option;
+	}
+
 	public String getDescription() {
 		return description;
 	}
